caa173bd0a2d61a7aac4fbc4d7ccae2070ff5ced
RosuAdrianMihai <rosumihai16@gmail.com>
2022-03-03 20:52:50 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Be Vietnam Pro", "sans-serie"]
+    },
+    colors: {
+      "bright-red":"hsl(12, 88%, 59%)",
+      "buttons-text-light-gray":"hsl(0, 0%, 98%)",
+      "paragraphs-grayish-blue":"hsl(227, 12%, 61%)",
+      "footer-dark-blue":"hsl(233, 12%, 13%)",
+      "headers-color":"hsl(228, 39%, 23%)",
+      "pale-red":"hsl(13, 100%, 96%)",
+      "white":"hsl(0,0%,100%)",
+      "black":"	hsl(0,0%,0%)",
+      "red":"	hsl(0,100%,50%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
